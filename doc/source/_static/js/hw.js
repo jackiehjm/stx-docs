722197e9e99037bf848d9a607ac1aaf15c878a24
Elisamara Aoki Goncalves <elisamaraaoki.goncalves@windriver.com>
2022-09-08 11:14:19 -0300

    TEST
    Branch test
    
    
    Signed-off-by: Elisamara Aoki Goncalves <elisamaraaoki.goncalves@windriver.com>
    Change-Id: I786c4356f5af40258139b58a67fc300ee1062c65

--- doc/source/_static/js/hw.js
+++ doc/source/_static/js/hw.js
@@ -17,12 +17,10 @@ if (page === 'verified-commercial-hardware.html') {
 
   // Strings
   document.body.innerHTML = document.body.innerHTML.replace(/Studio Cloud Version/g, 'StarlingX Version');
-  document.body.innerHTML = document.body.innerHTML.replace(/21.05/g, '5.0');
   document.body.innerHTML = document.body.innerHTML.replace(/21.12/g, '6.0');
   document.body.innerHTML = document.body.innerHTML.replace(/22.06/g, '7.0');
   document.body.innerHTML = document.body.innerHTML.replace(/WRCP 22.06/g, 'StarlingX 7.0');
   document.body.innerHTML = document.body.innerHTML.replace(/WRCP 21.12/g, 'StarlingX 6.0');
-  document.body.innerHTML = document.body.innerHTML.replace(/WRCP 21.05/g, 'StarlingX 5.0');
   document.body.innerHTML = document.body.innerHTML.replace(/Patch \d+/g, '');
 
 }
