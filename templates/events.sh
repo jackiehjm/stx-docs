efa7c5ab7d863f9dd0d6d4092f2de537174ed65d
Ron Stone <ronald.stone@windriver.com>
2022-11-04 14:10:22 -0400

    Download files
    Pull arbitrary files into the build.
    Use swapable configs to specify source and dest.
    Remove echo used for testing.
    Clean up comments
    Clean up usage output
    Add missing semicolon
    Standardize arg handling
    Add flag to skip branch lookup & dl from non-git URLs
    Delete commented/obsolete lines
    Troubleshoot zuul -1:
    Failing on "grep '^\[testenv:pdf-docs\]' src/opendev.org/starlingx/docs/tox.ini"
    - this change does not touch tox.ini
    - other reviews are passing with the same tox file
    - removing new subdir named 'configs' on hunch that this may be a
      reserved name?
    - Adjust linters to relax line lenght restrition on yaml files
    Update target location for alarms and logs.
    Delete temporary events dl used in testing
    
    Signed-off-by: Ron Stone <ronald.stone@windriver.com>
    Change-Id: I8ffff95688bf9a3a93e7e5a01bacbf132abe312d

---
+++ templates/events.sh
@@ -0,0 +1,13 @@
+# If set, will be prepended to output paths
+common_target="doc/source/fault-mgmt/kubernetes/"
+
+# The repo to download from
+remote_repo="opendev.org/starlingx/fault/raw/branch"
+
+# Associate remote files and local paths
+# key is the path to target in remote repo, relative to repo root
+# value is path and name to be saved, relative to CWD and common_target if set
+
+remote_files=(
+    ["fm-doc/fm_doc/events.yaml"]="test-events.yaml"
+    )
